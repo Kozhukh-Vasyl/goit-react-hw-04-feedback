8de5bcc5c0363175292286fdbce16fcf1e93ba49
Kozhukh-Vasyl <v.kozhukh@gmail.com>
2023-05-07 22:27:42 +0200

    add files

---
+++ src/components/App.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const AppContainer = styled.div`
+  padding: 14px;
+  margin: 0 auto;
+  width: 450px;
+  background-color: #f2fade;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
